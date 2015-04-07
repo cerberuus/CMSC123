f64b70fcd8be7750be44657e0ca51b29ee5e334b
Evan James Tingcoy <allfor.mir@gmail.com>
2015-04-07 22:54:38 +0800

    update

---
+++ PJ01/Pixel.java
@@ -0,0 +1,54 @@
+public class Pixel{
+	
+	private short red, green, blue;
+
+	public short Pixel(){
+		this.red = 0;
+		this.green = 0;
+		this.blue = 0;
+	}
+	public short Pixel(short red, short green, short blue){
+		this.red = red;
+		this.green = green;
+		this.blue = blue;
+	}
+
+	public short getRed(){
+		return this.red;
+	}
+
+	public short getGreen(){
+		return this.green;
+	}
+
+	public short getBlue(){
+		return this.blue;
+	}
+
+	public short setRed(short red){
+		return this.red = red;
+	}
+
+	public short setGreen(short green){
+		return this.green = green;
+	}
+
+	public short setBlue(short blue){
+		return this.blue = blue;
+	}
+
+	public String toString(int x, int y, short blue, short red, short green){
+		// int x is for the width while int y is for the height
+
+		String str = new String("");
+		for(int i = 0; i < x; i++)
+			for(int j = 0; j < y; j++){
+				str += "R" + setRed(red);
+				str += " G" + setGreen(green);
+				str += " B" + setBlue(blue) + "\n";
+			}
+
+		return str;
+
+	}
+}
